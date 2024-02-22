90b94f227ff831fcd1606dee72a0c7eab876618d
Pernilla Sterner <psterner@hotmail.com>
2024-02-22 15:24:28 +0100

    Make the code DRY. Add toogle to dropdown and display options for each recipe type

--- js/loaders.js
+++ js/loaders.js
@@ -1,20 +1,28 @@
-const recipyStoryLoader = (recipy) => {
-  const recipyStory = document.getElementById("big-story");
+// Get references to HTML elements using their IDs
+const recipeStory = document.getElementById("big-story");
+const recipeWrapper = document.getElementById("section__recipes");
+const cuisineDropdown = document.getElementById("cuisine");
+const authorDropdown = document.getElementById("author");
+const filterAuthors = document.getElementById("filterAuthors");
+const filterCuisines = document.getElementById("filterCuisines");
+const filterDropdown = document.querySelector(".filterDropdown");
 
-  // Add first recipy to top stories
-  recipyStory.innerHTML += `
+// Function to load and display the list of recipe
+const loadRecipeStory = (recipe) => {
+  // Add first recipe to top stories
+  recipeStory.innerHTML += `
    <div class="content">
      
      <img
-     src=${recipy.image}
-     alt="Recipy Image"
+     src=${recipe.image}
+     alt="recipe Image"
      height="auto"
      />
  
      <div class="description">
-       <h3>${recipy.name}</h3>
+       <h3>${recipe.name}</h3>
        <hr />
-       <p><span>Cuisine:</span> ${recipy.cuisineType
+       <p><span>Cuisine:</span> ${recipe.cuisineType
          .map((cuisine) => cuisine)
          .join("")}</p>
        <p><span>Time:</span> 2h 10min</p>
@@ -24,37 +32,34 @@ const recipyStoryLoader = (recipy) => {
    <div class="ingredients">
      <h3>Ingredients</h3>
      <ul>
-     ${recipy.ingredients
+     ${recipe.ingredients
        .map((ingredient) => `<li>${ingredient}</li>`)
        .join("")}
      </ul>
    </div>
    `;
 };
-recipyStoryLoader(recipes[0]);
 
 // Function for loading and displaying recipes
-const recipyLoader = (recipes) => {
-  const recipyWrapper = document.getElementById("section__recipes");
+const loadRecipes = (recipes) => {
+  recipeWrapper.innerHTML = "";
 
-  recipyWrapper.innerHTML = "";
-
-  recipes.forEach((recipy) => {
-    recipyWrapper.innerHTML += `
-    <div class="recipy-wrapper">
+  recipes.forEach((recipe) => {
+    recipeWrapper.innerHTML += `
+    <div class="recipe-wrapper">
       <img
-      src=${recipy.image}
-        alt="Recipy Image"
+      src=${recipe.image}
+        alt="recipe Image"
         height="auto"
       />
-      <h3>${recipy.name}</h3>
-        <p><span>Cuisine: ${recipy.cuisineType}</span></p>
+      <h3>${recipe.name}</h3>
+        <p><span>Cuisine: ${recipe.cuisineType}</span></p>
         <p><span>Time: 1h 20min</span></p>
-        <p>${recipy.author}</p>
+        <p>${recipe.author}</p>
       <hr />
       <h3>Ingredients</h3>
       <ul>
-      ${recipy.ingredients
+      ${recipe.ingredients
         .map((ingredient) => `<li>${ingredient}</li>`)
         .join("")}
       </ul>
@@ -63,63 +68,66 @@ const recipyLoader = (recipes) => {
     `;
   });
 };
-recipyLoader(recipes);
 
-const getSelectedFilter = (filterName, selectedFilters, recipes) => {
-  const filteredRecipes = recipes.filter((recipe) => {
-    if (filterName === "cuisine") {
-      return recipe.cuisineType.some((cuisine) =>
-        selectedFilters.includes(cuisine.toLowerCase())
-      );
-    } else {
-      return selectedFilters.includes(recipe.author.toLowerCase());
+// Function to fetch options for filtering recipies
+const fetchFilterOptions = (targetId) => {
+  // Need to adjust cuisine -> Cuisine
+  // Om jag klickar på author så använder jag target id för att trigga dropdown
+  const newTargetId = targetId.charAt(0).toUpperCase() + targetId.slice(1);
+  const filterDropdown = document.getElementById("filter" + newTargetId);
+
+  const getTypes = (targetId) => {
+    const typeSet = new Set();
+
+    if (targetId === "cuisine") {
+      recipes.forEach((recipe) => {
+        recipe.cuisineType.forEach((cuisineType) => {
+          typeSet.add(cuisineType.toLowerCase());
+        });
+      });
+      // Toogle the dropdown
+      filterDropdown.classList.toggle("active");
+    } else if (targetId === "author") {
+      recipes.forEach((recipe) => {
+        typeSet.add(recipe.author.toLowerCase());
+      });
+      // Toogle the dropdown
+      filterDropdown.classList.toggle("active");
     }
-  });
 
-  recipyLoader(filteredRecipes.length ? filteredRecipes : recipes);
-};
-
-const filterLoader = () => {
-  // get all the checkboxes
-  displayCuisineCheckboxes(recipes);
-  displayAuthorCheckboxes(recipes);
-  const filterCuisines = document.querySelectorAll(".cuisine");
-  const filterAuthors = document.querySelectorAll(".author");
-
-  // Funktion to filter recipes by cuisine
-  const filterRecipesByCuisine = (filterName) => {
-    console.log(filterName);
-    const selectedFilters = Array.from(
-      filterName === "cuisine" ? filterCuisines : filterAuthors
-    )
-      .filter((filter) => filter.checked)
-      .map((filter) => filter.value);
-
-    getSelectedFilter(filterName, selectedFilters, recipes);
+    return [...typeSet].sort();
   };
 
-  let filterName = "";
+  const loadFilters = (types) => {
+    // Clean the dropdown
+    filterDropdown.innerHTML = "";
 
-  // Add eventlistners
-  filterCuisines.forEach((cuisine) => {
-    cuisine.addEventListener("click", (e) => {
-      filterName = e.target.name;
-      filterRecipesByCuisine(filterName);
+    types.forEach((type) => {
+      filterDropdown.innerHTML += `
+      <label>
+        <input type="checkbox" name="${targetId}" class="${targetId}" value="${type}"/>
+        ${type}
+      </label> `;
     });
-  });
+  };
 
-  filterAuthors.forEach((author) => {
-    author.addEventListener("click", (e) => {
-      filterName = e.target.name;
-      filterRecipesByCuisine(filterName);
-    });
-  });
+  loadFilters(getTypes(targetId));
 };
 
 // Function to filter and display recipes based on cuisine and author
-// const filterLoader = (recipes) => {
-//   cuisineLoader(recipes);
-//   authorLoader(recipes);
-// };
+const filterRecipes = (e) => {
+  // Get the id from the dropdown
+  const targetId = e.target.id;
 
-filterLoader();
+  // Need to get the checkboxes
+  fetchFilterOptions(targetId);
+};
+
+// Function to display the list of favorite dogs
+
+// Apply the filter when the user changes the dropdown selection
+cuisineDropdown.addEventListener("click", filterRecipes);
+authorDropdown.addEventListener("click", filterRecipes);
+// Load the initial list of dogs when the page loads
+loadRecipeStory(recipes[0]);
+loadRecipes(recipes);
