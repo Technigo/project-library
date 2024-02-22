624dc4eeb30d77f9384634a94bb3d70f8d793df1
izzibizz <contact@izabellind.com>
2024-02-22 08:14:31 -0800

    update added toggle

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 If you choose to create your own arrays with elements, just make sure that some
 of the properties make sense to filter on, and some to sort on.*/
 const destinationDiv = document.getElementById('load-destinations')
+const buttonTemp = document.getElementById('button-temp')
+const buttonRate = document.getElementById('button.rate')
+const dropDown = document.getElementById('filter-dropdown')
 
 
 const destinations = [
@@ -24,7 +27,7 @@ const destinations = [
     rating: 4.2,
     temperature: 25,
     description:
-      'Beautifully restored mediaeval buildings, breathtaking views around every corner and a giddy network of winding streets strewn with shops, bars and restaurants make for a perfect holiday spot.',
+      'Beautifully restored mediaeval buildings, breathtaking views around every corner and a giddy network of winding streets strewn with shops, bars and restaurants.',
     image: './destinations-images/Taormina-italy.jpg'
   },
   {
@@ -35,7 +38,7 @@ const destinations = [
     rating: 4.4,
     temperature: 35,
     description:
-      'Check out the 350-million-year-old, striped sandstone domes of the Bungle Bungle Range from ground level or in the air. Wander along ancient creek beds and through striking gorges that transform into a golden paradise late in the day.  ',
+      'Wander along ancient creek beds and through striking gorges that transform into a golden paradise late in the day.  ',
     image: './destinations-images/purnululu-aus.jpg'
   },
   {
@@ -46,7 +49,7 @@ const destinations = [
     rating: 4.0,
     temperature: 27,
     description:
-      'The colorful, flowery, and highly decorated trajineras are some of the distinctive features of this important place of the Mexican culture, where chinampas crops remain, and farmers move by sailing through the canals while enduring their traditions and celebrations.',
+      'The colorful, flowery, and highly decorated trajineras are some of the distinctive features of this important place of the Mexican culture.',
     image: './destinations-images/xochimilco-lake-mexico.jpg'
   },
   {
@@ -57,7 +60,7 @@ const destinations = [
     rating: 5.0,
     temperature: 39,
     description:
-      'Uluru is world famous for its amazing rock paintings. The rock art and countless petroglyphs around Ulurus caves have many layers of symbols and figures painted on top of each other because these are sites that have been used as a part of Anangu education for thousands of years.',
+      'Uluru is world famous for its amazing rock paintings. Its a site that has been used as a part of Anangu education for thousands of years.',
     image: './destinations-images/Uluru-australia.jpg'
   },
   {
@@ -68,7 +71,7 @@ const destinations = [
     rating: 4.6,
     temperature: 12,
     description:
-      'Drift between pure marble columns, through smooth arches that have been carved out over thousands of years. The water beneath is an almost luminescent turquoise, and the light reflects across the soft pinks, blues and greens that run through the marble.',
+      'Drift between pure marble columns, through smooth arches that have been carved out over thousands of years.',
     image: './destinations-images/marble-cathedral-chile.jpg'
   },
   {
@@ -79,7 +82,7 @@ const destinations = [
     rating: 5.0,
     temperature: 7,
     description:
-      'Characterized by spectacular red cliffs and a range of erosional landforms, including dramatic natural pillars, towers, ravines, valleys and waterfalls.',
+      'Characterized by spectacular red cliffs and a range of erosional landforms, Danxia is famous for its rainbow colors.',
     image: './destinations-images/danxia landform geological park china.jpg'
   },
   {
@@ -90,7 +93,7 @@ const destinations = [
     rating: 4.5,
     temperature: -7,
     description:
-      'This beautiful region is perfectly situated to host some of the most amazing Northern Lights viewing, as well as being home to fascinating natural wonders and vibrant Swedish Lapland culture.',
+      'Abisko host some of the most amazing Northern Lights viewing, as well as the vibrant Swedish Lapland culture.',
     image: './destinations-images/abisko-national-park-sweden.jpg'
   },
   {
@@ -101,18 +104,18 @@ const destinations = [
     rating: 4.9,
     temperature: 6,
     description:
-      'A sea of red: red houses, red Buddhist gowns is what you’ll find in Larung Gar Buddhist Academy. There are thousands of small red houses on the slope surrounding the academy. ',
+      'This town is famous for its Buddhist Academy that holds thousands of students, monks and nuns. The town is a sea of red houses.',
     image: './destinations-images/Larung-gar-Tibet.jpg'
   },
   {
     name: 'Wangxian Valley',
     country: 'China',
     language: 'Chinese',
-    accessibility: 'Fiction',
+    accessibility: 'Moderate',
     rating: 4.1,
     temperature: 8,
     description:
-      'Wangxian is famous for its houses hanging from its cliff. Integrating landscapes, outdoor rafting, culture and folk experience all in one.',
+      'Wangxian is famous for its houses hanging from its cliff. Integrating landscapes, outdoor rafting, culture and folk experience.',
     image: './destinations-images/wangxian-valley-china.jpg'
   },
 ]
@@ -120,53 +123,58 @@ const destinations = [
 //Skapa funktion som laddar array in i html
 const loadDestinations = (destinations) => {
     destinationDiv.innerHTML = ""
-    destinations.forEach((x) => {
+    destinations.forEach((object) => {
     destinationDiv.innerHTML += `
     <div class="container">
-      <h4>"${x.name}"<span>"${x.country}</span></h4>
-      <img src="${x.image}">
-      <div class="description-text"><p>"${x.description}"</p></div>
+      <div class="text-above"><h4>${object.name}<br><span>${object.country}</span></div>
+      <div class="img-container"><img src="${object.image}"></div>
+      <ul>. 
+        <li>Language: ${object.language}</li>
+        <li>Accessibility: ${object.accessibility}</li>
+        <li>Temperature: ${object.temperature}ºC</li>
+        <li>Rating: ${object.rating}/5</li>
+      </ul>
+      <div class="description-text"><p>${object.description}</p></div>
       </div>
     `
-    console.log(destinations)
-    console.log(destinationDiv.innerHTML)
     })
 }
-
+// hämtar alla resmål när man laddar sidan
 loadDestinations(destinations)
 
+//sätt högst upp under DOM
+let lowToHigh = true
 
-   
+const tempToggle = () => {
+  lowToHigh = !lowToHigh
+  sortBy()
+}
+
+const sortBy = () => {
+  if (lowToHigh) {
+  destinations.sort((a, b) => {
+      return b.temperature - a.temperature
+  })
+  } else {
+    destinations.sort((a, b) => {
+      return a.temperature - b.temperature
+    })
+}
+loadDestinations(destinations)
+}
+
+// alla addEventListeners tillsammans på botten sen
+buttonTemp.addEventListener("click", tempToggle)
+
 
 
 
 /*
-const loadPokemons = (pokemons) => {  
-  pokeCard.innerHTML = ''
-  favorites.innerHTML = ''
-//Structure of the Pokémon library
-  pokemons.forEach((pokemon) => {
-    pokeCard.innerHTML += `
-    <div class="card">
-    <h3>${pokemon.name}</h3>
-            <span>${pokemon.type}</span>
-    <div class="content-container">
-      <div class="image-container">
-        <img src="${pokemon.image}" alt="${pokemon.name}" class="poke-img"/>
-      </div>
-      <div class="text-container">
-      <span>Category: ${pokemon.category}</span>
-        <p>${pokemon.description}</p>
-        <ul class="list">
-          <li>HP: ${pokemon.HP}</li>
-          <li>Attack: ${pokemon.attack}</li>
-          <li>Defense: ${pokemon.defense}</li>
-          <li>Speed: ${pokemon.speed}</li>
-        </ul>
-      </div>
-      </div>
-      `
+
+const sortBy = (temperature)
+  destinations.sort(function(a, b) => {
+    if (temperature(a) < temperature(b))
+    return b - a;
   })
-}
-      
-      */
+  
+*/
